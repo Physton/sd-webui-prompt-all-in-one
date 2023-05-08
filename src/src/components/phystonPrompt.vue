57f5faac321dd8fe40ef89893c6cbb1b8f34b0e6
Physton <physton@163.com>
2023-05-09 01:30:57 +0800

    bug fix
    Fix the bug of continuous loading when clicking on the "Translate All" button without keywords.

--- src/src/components/phystonPrompt.vue
+++ src/src/components/phystonPrompt.vue
@@ -641,6 +641,7 @@ export default {
             })
         },
         onTranslatesToLocalClick() {
+            if (this.tags.length === 0) return
             if (this.loading['all_local']) return
             this.loading['all_local'] = true
             let tagIndexes = []
@@ -675,6 +676,7 @@ export default {
             })
         },
         onTranslatesToEnglishClick() {
+            if (this.tags.length === 0) return
             if (this.loading['all_en']) return
             this.loading['all_en'] = true
             let tagIndexes = []
