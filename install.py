2b59fe0d8e3ee136cf77ce9dd087b63119f1c36a
Physton <physton@163.com>
2023-05-12 00:43:12 +0800

    Update install.py

--- install.py
+++ install.py
@@ -11,7 +11,8 @@ packages = {
     "tencentcloud": "tencentcloud-sdk-python",
 }
 
-for package_name, package in packages.items():
+for package_name in packages:
+    package = packages[package_name]
     try:
         if not launch.is_installed(package_name):
             launch.run_pip(f"install {package}", f"sd-webui-prompt-all-in-one: {package_name}")
