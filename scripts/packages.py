42cb44d74986e78562bdf2e3cbac45ddf40201ae
Physton <physton@163.com>
2023-05-29 11:35:07 +0800

    Fix the issue #98 #99
    Former-commit-id: ec301ea8136555a2b563c0b5c3c4e283e3c61cc9

--- scripts/packages.py
+++ scripts/packages.py
@@ -1,11 +1,17 @@
 import launch
-import importlib
-import os
 
-install_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../install.py')
-install_file = os.path.normpath(install_file)
-install = importlib.import_module('install', install_file)
-packages = install.packages
+packages = {
+    "chardet": "chardet",
+    "fastapi": "fastapi",
+
+    # The following packages are required for translation service. If you do not need translation service, you can remove them.
+    # 以下是翻译所需的包，如果不需要翻译服务，可以删除掉它们。
+    "translators": "translators",
+    "openai": "openai",
+    "boto3": "boto3",
+    "aliyunsdkcore": "aliyun-python-sdk-core",
+    "aliyunsdkalimt": "aliyun-python-sdk-alimt",
+}
 
 def get_packages_state():
     states = []
