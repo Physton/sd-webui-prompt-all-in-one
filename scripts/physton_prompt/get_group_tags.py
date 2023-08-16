97b3279e8e297c21330af71ba5642d61e2004059
Physton <physton@163.com>
2023-08-16 10:59:48 +0800

    Keyword group supports custom append content file: append.yaml
    关键词组支持自定义追加内容文件：append.yaml

--- scripts/physton_prompt/get_group_tags.py
+++ scripts/physton_prompt/get_group_tags.py
@@ -1,21 +1,33 @@
 import os
 
-def get_group_tags(lang):
-    current_dir = os.path.dirname(os.path.abspath(__file__))
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
-    tags_file = os.path.join(current_dir, '../../group_tags/', 'custom.yaml')
+def _get_tags_filename(name):
+    file = os.path.join(current_dir, '../../group_tags/', name + '.yaml')
+    return file
+
+def get_group_tags(lang):
+    tags_file = _get_tags_filename('custom')
     if not os.path.exists(tags_file):
-        tags_file = os.path.join(current_dir, '../../group_tags/', lang + '.yaml')
+        tags_file = _get_tags_filename(lang)
         if not os.path.exists(tags_file):
-            tags_file = os.path.join(current_dir, '../../group_tags/', 'default.yaml')
+            tags_file = _get_tags_filename('default')
     if not os.path.exists(tags_file):
         return ''
 
-    tags_file = os.path.normpath(tags_file)
     tags = ''
     try:
         with open(tags_file, 'r', encoding='utf8') as f:
             tags = f.read()
     except:
         pass
+
+    try:
+        append_file = _get_tags_filename('append')
+        with open(append_file, 'r', encoding='utf8') as f:
+            append = f.read()
+        tags += "\n\n" + append
+    except:
+        pass
+
     return tags
